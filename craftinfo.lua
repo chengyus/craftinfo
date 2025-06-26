a9e2b6ac9529f4883fcae1762c936813689f1960
Joseph Sung <chengyu.sung@gmail.com>
2025-06-26 14:21:30 +0800

    seems to get what I want from separate parser script; committing first to test in game

--- craftinfo.lua
+++ craftinfo.lua
@@ -6,26 +6,66 @@ addon.desc      = 'Tries to retrieve craft info.';
 addon.link      = 'N/A atm';
 
 require 'common'
-require 'socket.http'
-local json = require 'json'
+local http = require 'socket.http'
+-- local json = require 'json'
+local chat = require('chat');
 
-ashita.events.register('command', 'command_cb', function(cmd, nType)
-  if cmd:lower():startswith('/craftinfo ') then
-    local item = cmd:sub(12)
-    local url = 'https://bg-wiki.com/api.php?action=query&titles='
-      .. ashita.ffxi.encode_uri(item)
-      .. '&prop=extracts&format=json&exintro=1'
-    local body, code = http.request(url)
-    if code == 200 then
-      local js = json.decode(body)
-      local pages = js.query.pages
-      for _, page in pairs(pages) do
-        ashita.chat.output(page.extract or 'No info found.')
-      end
-    else
-      ashita.chat.output('HTTP error: ' .. tostring(code))
-    end
+local craftinfo = T{
+  item    = '           ',  -- The item/receipe to lookup.
+  recipes = {}
+};
+
+--local function encode_uri(str)
+--  if not str then
+--    return nil
+--  end
+--  str = string.gsub(str, "([^%w%.%-]+)", function (c)
+--    return string.format("%%%02X", string.byte(c))
+--  end)
+--  return str
+--end
+
+local function parse_bg_synth_info(body, item)
+  first, last = string.find(body, item);
+  --print("item match first: "..first);
+  --print("item match last: "..last);
+  local htmlparser = require("htmlparser");
+  local root = htmlparser.parse(body);
+  local elements = root:select(".item-info-body");
+  for _,e in ipairs(elements) do
+	  --print(e.name)
+	  local subs = e("li::marker > a:not(.selflink)")
+	  for _,sub in ipairs(subs) do
+		  print(sub:getcontent())
+	  end
+  end
+end
+
+ashita.events.register('command', 'command_cb', function(e)
+  local args = e.command:args();
+  local cmd, item;
+  if (#args == 0 or not args[1] == '/craftinfo') then
+    print(chat.header(addon.name):append(chat.message('[Error]: command incorrect.')));
+    return;
+  end
+  cmd = e.command;
+  --print(#args);
+  --print("\n");
+  --print(args[2]);
+  if (#args == 2) then
+    craftinfo.item = args[2];
+    -- print(chat.header(addon.name):append(chat.message('[DEBUG]: item set to: ')):append(chat.success(craftinfo.item)));
+  end
+  local item_ = craftinfo.item:gsub("%s+", "_");
+  -- print(item_);
+  local url = 'https://bg-wiki.com/ffxi/' .. item_;
+  local body, code = http.request(url)
+  if code == 200 then
+    -- print("200 info found");
+    parse_bg_synth_info(body, craftinfo.item);
     return true
+  else
+    ashita.chat.output('HTTP error: ' .. tostring(code))
   end
   return false
 end)
